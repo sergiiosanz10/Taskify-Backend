a36010af4555f48bcc710e696b1a2ec43e37a114
Sergio Sanz <sergiosanz2004@gmail.com>
2024-05-12 20:42:15 +0200

    a

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:4200'
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
